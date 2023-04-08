cca83ae0549ee419b70943acfa5af25afbff38c5
SEMIKENKE Deogratias <120032594+SemiLwanga@users.noreply.github.com>
2023-04-08 23:33:51 +0200

    ajout de la classe "payement"

--- code_client.py
+++ code_client.py
@@ -31,4 +31,42 @@ class detailCall:
             a = i.split('|')
             self.res.append(dict(zip(b, a)))
             
+class Payement:
+    
+    def __init__(self,liste_dict):
+        self.liste_dict = liste_dict
+        self.amount = 0
+    
+    def description(self):
+        
+        for i in self.liste_dict:
+            type_call = float(i["type d'appel"])
+            num = i['appelé']
+            if (i['durée'] != ''):
+            	tempsAppel = float(i['durée'])
+            else:
+            	tempsAppel = 0
+            total_vol = float(i['totalVolume'])
+            taxe = float(i['taxe'])
+            amount = 0
+            if type_call == 0:
+                if (num[0:4] == '24382' or num[0:4] == '24381'):
+                    amount += 0.025*tempsAppel/60
+                else:
+                    amount += 0.05*tempsAppel/60
+            elif (type_call == 1):
+                if (num[0:4] == '24382' or num[0:4] == '24381'):
+                    amount  += 0.001
+                else:
+                    amount += 0.002
+            else:
+                amount += total_vol*0.03
+            if (taxe == 1):
+                amount =amount + amount *5/100
+            elif (taxe == 2):
+                amount = amount + amount *16/100
+            self.amount += amount
+            
+            
+                 
             
